bf2f825f68d5738aacf6e87766db3d810d4efdfc
jeremy_renevier <renevier.jeremy@gmail.com>
2021-01-13 11:06:55 +0100

    Add trigonometrie declaration to 'MathLibrary.h'

---
+++ MathLibrary/MathLibrary.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "MathLibrary.h"
+
+double Sin(unsigned _a)
+{
+}
+
+double Cos(unsigned _a)
+{
+}
+
+double Tan(unsigned _a)
+{
+}
--- MathLibrary/MathLibrary.h
+++ MathLibrary/MathLibrary.h
@@ -5,3 +5,17 @@
 #else
 #define MATHLIBRARY_API __declspec(dllimport)
 #endif
+
+//trigonometrie
+
+MATHLIBRARY_API double Sin(unsigned _a);
+MATHLIBRARY_API double Cos(unsigned _a);
+MATHLIBRARY_API double Tan(unsigned _a);
+MATHLIBRARY_API double ASin(unsigned _a);
+MATHLIBRARY_API double ACos(unsigned _a);
+MATHLIBRARY_API double ATan(unsigned _a);
+MATHLIBRARY_API double ATan2(unsigned _a);
+MATHLIBRARY_API float PI();
+MATHLIBRARY_API float InvPI();
+MATHLIBRARY_API double DegToRad(double _a);
+MATHLIBRARY_API double RadToDeg(double _a);
