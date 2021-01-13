f401c861961961b859756aa794821ea195dd6c6d
GA\j.renevier <renevier.jeremy@gmail.com>
2021-01-13 15:08:47 +0100

    add trigo function

--- MathLibrary/MathLibrary.cpp
+++ MathLibrary/MathLibrary.cpp
@@ -1,14 +1,97 @@
 #include "pch.h"
 #include "MathLibrary.h"
 
-double Sin(unsigned _a)
+float PI()
 {
+	return 3.141592653589793238462643383279502884;
 }
 
-double Cos(unsigned _a)
+float InvPI()
 {
+	return 0.0f;
 }
 
-double Tan(unsigned _a)
+double DegToRad(double _a)
 {
+	return _a * (PI() / 180);
+}
+
+double RadToDeg(double _a)
+{
+	return _a *(180/PI());
+}
+
+double Sin(double _x)
+{
+	double sign = 1;
+
+	if (_x < 0) {
+		sign = -1.0;
+		_x = -_x;
+	}
+
+	if (_x > 360) _x -= int(_x / 360) * 360;
+		_x *= PI() / 180.0;
+
+	double res = 0;
+	double term = _x;
+	int k = 1;
+
+	while (res + term != res)
+	{
+		res += term;
+		k += 2;
+		term *= -_x * _x / k / (k - 1);
+	}
+
+	return sign * res;
+}
+
+double Cos(double _x)
+{
+	if (_x < 0) 
+		_x = -_x;
+
+	if (_x > 360)
+		_x -= int(_x / 360) * 360;
+
+	_x *= PI() / 180.0;
+
+	double res = 0;
+	double term = 1;
+	int k = 0;
+
+	while (res + term != res) 
+	{
+		res += term;
+		k += 2;
+		term *= -_x * _x / k / (k - 1);
+	}
+
+	return res;
+}
+
+double Tan(double _x)
+{
+	return;
+}
+
+double ASin(unsigned _a)
+{
+	return;
+}
+
+double ACos(unsigned _a)
+{
+	return;
+}
+
+double ATan(unsigned _a)
+{
+	return;
+}
+
+double ATan2(unsigned _a)
+{
+	return;
 }
--- MathLibrary/MathLibrary.h
+++ MathLibrary/MathLibrary.h
@@ -8,14 +8,14 @@
 
 //trigonometrie
 
-MATHLIBRARY_API double Sin(unsigned _a);
-MATHLIBRARY_API double Cos(unsigned _a);
-MATHLIBRARY_API double Tan(unsigned _a);
-MATHLIBRARY_API double ASin(unsigned _a);
-MATHLIBRARY_API double ACos(unsigned _a);
-MATHLIBRARY_API double ATan(unsigned _a);
-MATHLIBRARY_API double ATan2(unsigned _a);
 MATHLIBRARY_API float PI();
 MATHLIBRARY_API float InvPI();
 MATHLIBRARY_API double DegToRad(double _a);
 MATHLIBRARY_API double RadToDeg(double _a);
+MATHLIBRARY_API double Sin(double _x);
+MATHLIBRARY_API double Cos(double _x);
+MATHLIBRARY_API double Tan(double _x);
+MATHLIBRARY_API double ASin(unsigned _a);
+MATHLIBRARY_API double ACos(unsigned _a);
+MATHLIBRARY_API double ATan(unsigned _a);
+MATHLIBRARY_API double ATan2(unsigned _a);
